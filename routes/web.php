4b13fb4f30f3776c38307b8818476d8e4fd13b28
Mayank0851 <89372941+Mayank0851@users.noreply.github.com>
2024-10-17 17:32:19 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return dd("Test123456789");
+    return dd("test123456789");
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
