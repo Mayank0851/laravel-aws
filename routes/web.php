80256f2f166d3c0da738ddefde5b667d11e76d4c
Mayank0851 <89372941+Mayank0851@users.noreply.github.com>
2024-10-21 14:57:11 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome to laravel app'), [
+    return Inertia::render('Welcome'), [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
